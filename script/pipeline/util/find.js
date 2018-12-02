79361f6a92c8ea1182ea57e0b8fb7a1b976126d7
Titus Wormer <tituswormer@gmail.com>
2018-12-02 19:51:36 +0100

    Move emiritus team outside of members

--- script/pipeline/util/find.js
+++ script/pipeline/util/find.js
@@ -8,10 +8,16 @@ function find(ctx, expression) {
   var value = interpolate(ctx, expression)
   var [name, role] = value.split('/')
   var team = ctx.teams.find(x => x.name === name)
+  var modify = identity
   var members
 
   if (!team) {
     throw new Error('Could not find team `' + name + '`')
+  }
+
+  if (role.charAt(0) === '!') {
+    modify = negate
+    role = role.slice(1)
   }
 
   members = team.members
@@ -19,7 +25,15 @@ function find(ctx, expression) {
   return Object.keys(members).filter(
     member =>
       role === '*' ||
-      members[member] === role ||
+      modify(members[member] === role) ||
       (role === 'lead' && team.lead === member)
   )
+}
+
+function identity(x) {
+  return x
+}
+
+function negate(x) {
+  return !x
 }
--- script/structure.js
+++ script/structure.js
@@ -1,28 +1,27 @@
 module.exports = [
   {
-    name: 'members',
-    description: 'All @:org members, active or inactive',
-    repositories: {scope: ':org/*', right: 'pull'},
-    humans: {member: ':orgTeam/*', maintainer: ':orgTeam/admin'}
-  },
-  {
-    name: 'emeriti',
-    parent: 'members',
-    description: 'All inactive @:org members',
+    name: 'emeritus',
+    description: 'Past members of the @:org organization team',
     repositories: {scope: ':org/*', right: 'pull'},
     humans: {member: ':orgTeam/emeritus', maintainer: ':orgTeam/admin'}
   },
   {
+    name: 'members',
+    description: 'The @:org organization team',
+    repositories: {scope: ':org/*', right: 'pull'},
+    humans: {member: ':orgTeam/!emeritus', maintainer: ':orgTeam/admin'}
+  },
+  {
     name: 'mergers',
     parent: 'members',
-    description: 'All active @:org members with write rights',
+    description: '@:org members with write rights',
     repositories: {scope: ':org/*', right: 'push'},
     humans: {member: ':orgTeam/write', maintainer: ':orgTeam/admin'}
   },
   {
     name: 'releasers',
     parent: 'members',
-    description: 'All active @:org members with release rights',
+    description: '@:org members with release rights',
     repositories: {scope: ':org/*', right: 'admin'},
     humans: {member: ':orgTeam/admin', maintainer: ':orgTeam/lead'}
   },
